13b53aab5d2cdf1a1dc6674739bb04fe7cb9fbf0
zaimovkod@abv.bg <zaimovkod@abv.bg>
2023-01-12 11:24:55 +0200

    ot 1 do n prez 3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Дай число:");
+            var n = int.Parse(Console.ReadLine());
+            for (var i = 0; i < n; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
