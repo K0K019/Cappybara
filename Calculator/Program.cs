ae9168b22ab3832aef633b332768e739a1cdbcf7
Nikola Luchev <nikolakychev1923@gmail.com>
2023-02-08 10:06:42 +0200

    Calculator
    calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведи число");
+            var x = double.Parse(Console.ReadLine());
+            var y = double.Parse(Console.ReadLine());
+            Console.Write("Въведи +; -; *;/");
+            string n = Console.ReadLine();
+            switch (n)
+            {
+
+            }
+
+        }
+    }
+}
